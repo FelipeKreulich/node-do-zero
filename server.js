9a0c4f3a2ed00ee2b4ec8951be1618efe032618d
FelipeKreulich <felipek2002k@gmail.com>
2023-09-20 19:46:21 -0400

    feat: create HTTP native server

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import { createServer } from 'node:http';
 
-const server = createServer((request, response) => {
-  
-});
+const server = createServer((request, response) => {});
 
 server.listen(3333);
