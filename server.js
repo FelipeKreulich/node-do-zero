21ed79b6158276b837482b1a2bde9203ab19edb5
FelipeKreulich <felipek2002k@gmail.com>
2023-09-20 19:54:10 -0400

    feat: added Fastify to the project

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-import { createServer } from 'node:http';
+// import { createServer } from 'node:http';
 
-const server = createServer((request, response) => {});
+// const server = createServer((request, response) => {
 
-server.listen(3333);
+// });
+
+// server.listen(3333);
+
+import { fastify } from 'fastify';
+
+const server = fastify();
+
+server.get('/', () => {
+  return 'Hello World'
+});
+
+server.listen({
+  port: 3333,
+});
