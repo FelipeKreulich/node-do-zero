a59bc1726522009d5de4410aff057f12dbc6174c
FelipeKreulich <felipek2002k@gmail.com>
2023-09-21 23:30:19 -0400

    feat: create Methods HTTP Get and Post

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Criando servidor HTTP nativo com NodeJS
 // import { createServer } from 'node:http';
 
 // const server = createServer((request, response) => {
@@ -6,14 +7,39 @@
 
 // server.listen(3333);
 
-import { fastify } from 'fastify';
+import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
 
-const server = fastify();
+const server = fastify()
 
-server.get('/', () => {
-  return 'Hello World'
+const database = new DatabaseMemory()
+
+server.get('/videos', () => {
+  const videos = database.list()
+
+  return videos
 });
+
+server.post('/videos', (request, reply) => {
+  const { title, description, duration } = request.body
+
+  database.create({
+    title,
+    description,
+    duration,
+  })
+
+  return reply.status(201).send()
+})
+
+server.put('/videos/:id', () => {
+  
+})
+
+server.delete('/videos/:id', () => {
+  
+})
 
 server.listen({
   port: 3333,
-});
+})
